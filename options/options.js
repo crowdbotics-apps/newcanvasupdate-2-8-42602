d77774ae299326f5c4486840cbdf63c6bfc438cb
Crowdbotics <team@crowdbotics.com>
2023-08-02 23:16:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "newcanvasupdate-2-8-42602",
+  url: "https://newcanvasupdate-2-8-42602.botics.co",
+  api: "https://newcanvasupdate-2-8-42602.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
